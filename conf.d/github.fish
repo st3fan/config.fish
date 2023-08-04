2e97a7015bd40a23a5a4ad62789a0f9cd4993ff1
Stefan Arentz <stefan@arentz.ca>
2023-08-04 16:23:48 -0400

    Updates

---
+++ conf.d/github.fish
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+# This actually confuses some tools. Better to use .env file and op run --env-file=
+#export GITHUB_TOKEN="op://Private/GitHub Token Minimal/credential"
+
+
---
+++ conf.d/theme.fish
@@ -0,0 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+# TODO Do something smart here to switch between Latte and Mocha
+fish_config theme save "Catppuccin Latte"
+

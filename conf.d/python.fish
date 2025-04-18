70488f31341138d533bfdf9fc7517c484051a1d2
Stefan Arentz <stefan@arentz.ca>
2025-04-18 15:19:55 -0400

    Updates

--- conf.d/python.fish
+++ conf.d/python.fish
@@ -3,7 +3,20 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 if status is-interactive
-  if type -q pip
-    pip completion --fish | source
-  end
+    if type -q pip
+        pip completion --fish | source
+    end
+
+    #if type -q pipx
+    #  register-python-argcomplete --shell fish pipx | source
+    #end
+
+    # Why does this need to be here? Is this not done first in config.fish?
+    if test -d $HOME/.local/bin
+        set -gx PATH $HOME/.local/bin $PATH
+    end
+
+    if type -q poetry
+        poetry completions fish | source
+    end
 end
